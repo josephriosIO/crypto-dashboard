2b29acb3ce9a78419ee2f0f62ccf7d1b177e4f46
josephriosio <blankworldwide@gmail.com>
2019-07-16 21:08:14 -0700

    css grid

--- crypto-dash/src/App.js
+++ crypto-dash/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
 import "./App.css";
 
 function App() {
   return (
     <AppLayout className="App">
+      <AppBar />
       <h1>crypto</h1>
     </AppLayout>
   );
