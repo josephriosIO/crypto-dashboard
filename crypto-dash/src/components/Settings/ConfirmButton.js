167d60dcbfc0012162a8d08daf753b245b32d6f4
josephriosio <blankworldwide@gmail.com>
2019-07-16 23:13:50 -0700

    created coin grid

--- crypto-dash/src/components/Settings/ConfirmButton.js
+++ crypto-dash/src/components/Settings/ConfirmButton.js
@@ -1,20 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../../AppProvider";
+import { fontSize1, blueBoxShadow } from "../Shared/Styles";
 
 const ConfirmBtnStyled = styled.div`
   color: #45b3e7;
+  ${fontSize1};
   margin: 20px;
+  padding: 5px;
   cursor: pointer;
+  &:hover {
+    ${blueBoxShadow}
+  }
+`;
+
+export const CenterDiv = styled.div`
+  display: grid;
+  justify-content: center;
 `;
 
 export default function() {
   return (
     <AppContext.Consumer>
       {({ confirmFavs }) => (
-        <ConfirmBtnStyled onClick={confirmFavs}>
-          Confirm Favorites
-        </ConfirmBtnStyled>
+        <CenterDiv>
+          <ConfirmBtnStyled onClick={confirmFavs}>
+            Confirm Favorites
+          </ConfirmBtnStyled>
+        </CenterDiv>
       )}
     </AppContext.Consumer>
   );
