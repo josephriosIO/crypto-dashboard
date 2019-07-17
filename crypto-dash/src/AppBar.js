732c41558b24043041dbe402a2eedac6dc534659
josephriosio <blankworldwide@gmail.com>
2019-07-16 21:20:13 -0700

    app bar

--- crypto-dash/src/AppBar.js
+++ crypto-dash/src/AppBar.js
@@ -1,18 +1,40 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Bar = styled.div`
   display: grid;
+  margin-bottom: 40px;
   grid-template-columns: 180px auto 100px 100px;
 `;
+
+const Logo = styled.div`
+  font-size: 1.5rem;
+`;
+
+const ControlBtnElem = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #45b3e7;
+    `}
+`;
+
+function lowerCase(name) {
+  return name.charAt(0).toUpperCase() + name.substr(1);
+}
+
+function ControlButton({ name, active }) {
+  return <ControlBtnElem active={active}>{lowerCase(name)}</ControlBtnElem>;
+}
 
 export default function() {
   return (
     <Bar>
+      <Logo>Crypto Look</Logo>
       <div />
-      <div>Crypto Look</div>
-      <div>Dashboard</div>
-      <div>Settings</div>
+      <ControlButton active name="Dashboard" />
+      <ControlButton name="Settings" />
     </Bar>
   );
 }
