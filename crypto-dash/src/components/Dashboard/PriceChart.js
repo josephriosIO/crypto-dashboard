5b48565d523f3af4a8712995cd00946820526702
josephriosio <blankworldwide@gmail.com>
2019-07-18 17:12:21 -0700

    new theme

--- crypto-dash/src/components/Dashboard/PriceChart.js
+++ crypto-dash/src/components/Dashboard/PriceChart.js
@@ -3,6 +3,8 @@ import HighChartsConfig from "./HighChartsConfig";
 import { Tile } from "../Shared/Tile";
 import ReactHighCharts from "react-highcharts";
 import { AppContext } from "../../AppProvider";
+import HighChartsTheme from "./HighChartsTheme";
+ReactHighCharts.Highcharts.setOptions(HighChartsTheme);
 
 export default function() {
   return (
